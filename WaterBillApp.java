860ad8dd6656b2cb7424a1185428bc70fbe60455
vat03 <100333733+vat03@users.noreply.github.com>
2025-02-01 18:00:19 +0530

    Update WaterBillApp.java

--- WaterBillApp.java
+++ WaterBillApp.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 public class WaterBillApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Enter the number of units consumed: ");
+
+		System.out.print("Enter the number of units consumed: ");
 		int unitsConsumed = scanner.nextInt();
-		
+
 		int meterCharge = 75;
 		int charge = 0;
-		
-		if(unitsConsumed <= 100)
-		{
-			charge = unitsConsumed * 5;
-		}
-		else
-		{
-			if(unitsConsumed <= 250)
-			{
-				charge = unitsConsumed * 10;
+
+		if (unitsConsumed >= 0) {
+			if (unitsConsumed <= 100) {
+				charge = unitsConsumed * 5;
+			} else {
+				if (unitsConsumed <= 250) {
+					charge = unitsConsumed * 10;
+				} else {
+					charge = unitsConsumed * 20;
+				}
 			}
-			else
-			{
-				charge = unitsConsumed * 20;
-			}
+			int totalWaterBill = charge + meterCharge;
+
+			System.out.println("\nUnit charge: "+charge);
+			System.out.println("Meter charge: "+meterCharge);
+			System.out.println("\nTotal Water bill: " + totalWaterBill);
+
+		} else {
+			System.out.println("\nEnter valid number of unit consumed !!");
 		}
-		
-		int totalWaterBill = charge + meterCharge;
-		
-		System.out.println("Total Water bill is: "+totalWaterBill);
-		
+
 		scanner.close();
 	}
 }
