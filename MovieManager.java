c60d03e9644d8d97b864fe1b29999fabbf58e7c1
vat03 <100333733+vat03@users.noreply.github.com>
2025-02-09 22:56:30 +0530

    Update MovieManager.java

--- MovieManager.java
+++ MovieManager.java
@@ -1,69 +1,61 @@
 package com.aurionpro.model;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MovieManager {
-    private List<Movie> movies;
-    private static final String filePath = "data.bin";
+	private List<Movie> movies;
+	private static final String filePath = "movies.dat";
 
-    public MovieManager() {
-        movies = new ArrayList<>();
-        loadMovies();
-    }
+	public MovieManager() {
+		movies = new ArrayList<>();
+		loadMovies();
+	}
 
-    // Add a movie
-    public void addMovie(Movie movie) {
-        if (movies.size() >= 5) {
-            throw new RuntimeException("CapacityFullException: Cannot add more than 5 movies.");
-        }
-        movies.add(movie);
-        saveMovies();
-    }
+	public void addMovie(Movie movie) {
+		if (movies.size() < 5) {
+			movies.add(movie);
+			saveMovies();
+		}
+	}
 
-    // Clear all movies
-    public void clearMovies() {
-        movies.clear();
-        saveMovies();
-    }
+	public void clearMovies() {
+		movies.clear();
+		saveMovies();
+	}
 
-    // Get all movies
-    public List<Movie> getMovies() {
-        return movies;
-    }
+	public List<Movie> getMovies() {
+		return movies;
+	}
 
-    // Get the next available movie ID
-    public int getMovieId() {
-        return movies.size() + 1;
-    }
+	public int getMovieId() {
+		return movies.size() + 1;
+	}
 
-    // Load movies from file
-    public void loadMovies() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
-            movies = (List<Movie>) ois.readObject();
-        } catch (FileNotFoundException e) {
-            System.out.println("No saved movies found. Starting with an empty list.");
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error loading movies: " + e.getMessage());
-        }
-    }
+	public void loadMovies() {
+		File file = new File(filePath);
+		if (file.exists()) {
+			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+				movies = (List<Movie>) ois.readObject();
+			} catch (IOException | ClassNotFoundException e) {
+				// Ignored
+			}
+		}
+	}
 
-    // Save movies to file
-    public void saveMovies() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            oos.writeObject(movies);
-        } catch (IOException e) {
-            System.out.println("Error saving movies: " + e.getMessage());
-        }
-    }
+	public void saveMovies() {
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+			oos.writeObject(movies);
+		} catch (IOException e) {
+			// Ignored
+		}
+	}
 
-    // Delete all movies permanently
-    public void deleteAllMovies() {
-        clearMovies();
-        File file = new File(filePath);
-        if (file.exists()) {
-            file.delete();
-        }
-    }
+	public void deleteAllMovies() {
+		File file = new File(filePath);
+		if (file.exists()) {
+			file.delete();
+		}
+		movies.clear();
+	}
 }
